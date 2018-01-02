9e0e69d55feefc625e7cb1dd50f17e7045bebba5
NissanGoldberg <nissan.goldberg@gmail.com>
2018-01-02 14:40:10 +0200

    changes to date predicate, update #ofMacs/#ofLines, added clear data button

--- src/main/java/components/Attributes/HashRouters.java
+++ src/main/java/components/Attributes/HashRouters.java
@@ -2,12 +2,15 @@ package components.Attributes;
 
 import java.util.*;
 
+/**
+ * @author Moshe
+ */
+
 public class HashRouters<S,T>{
-
+	private int countOfRouters = 0;
 	private Hashtable<S, ArrayList<T>> routers;
-	
-	public static void main(String[] args) {
 
+	public static void main(String[] args) {
 
 	}
 
@@ -18,7 +21,7 @@ public class HashRouters<S,T>{
 	/**
 	 * This function add ArrayList of elements with one key into the hashTable
 	 * @param key - type of S. the key of the element in the hashTable
-	 * @param value - type of T. the value which has the key above. 
+	 * @param value - type of T. the value which has the key above.
 	 */
 	public void addElement(S key, T value)
 	{
@@ -27,8 +30,9 @@ public class HashRouters<S,T>{
 			n.add(value);
 			routers.put(key, n);
 		}
-		else		
+		else
 		{
+			countOfRouters++;
 			ArrayList<T> n = new ArrayList<T>();
 			n.add(value);
 			routers.put(key, n);
@@ -36,9 +40,9 @@ public class HashRouters<S,T>{
 	}
 
 	/**
-	 *
-	 * @param key
-	 * @param values
+	 *The funcion gets a key and ArrayList of objects for this key, and inserts the objects to the key in the exist hash Table.
+	 * @param key - The key of the list. Type of key must be as a type of keys of the HashTable
+	 * @param values - The objects that will be inserted in the list that belongs to the key
 	 */
 	public void addListElement(S key, ArrayList<T> values)
 	{
@@ -50,19 +54,20 @@ public class HashRouters<S,T>{
 		}
 		else
 		{
+			countOfRouters++;
 			routers.put(key, values);
 		}
 	}
 
 	/**
-	 * 
+	 *
 	 * @return all the hashTable.
 	 */
 	public Hashtable<S, ArrayList<T>> getRoutersHashTable()
 	{
 		return routers;
 	}
-		
+
 	/**
 	 * The function get key and integer k. The function returns maximum the k best elements of this key.
 	 * @param key - the key of the k elements.
@@ -74,23 +79,28 @@ public class HashRouters<S,T>{
 	{
 		ArrayList<T> allAppearance;
 		ArrayList<T> kBest = new ArrayList<T>();
-		
+
 		if(!routers.containsKey(key))
 			kBest = null;
 		else
 		{
 			allAppearance = routers.get(key);
 			Collections.sort(allAppearance, comp);
-			
+
 			for(int i = 0; i < allAppearance.size() && i < k; i++)
 			{
 				kBest.add(allAppearance.get(i));
 			}
 		}
-	
+
 		return kBest;
 	}
 
+	/**
+	 * The function merge between the hash Table of this class to the hash Table that the function gets.
+	 * @param addHash - Hash Table the will merge. The type of keys and type of values of the hash Table must be similar
+	 * 			to the type of keys and type of values of the hash Table in this class.
+	 */
 	public void mergeToHash(HashRouters<S, T> addHash)
 	{
 		//HashRouters<S, ArrayList<T>> mergeHash = new HashRouters<>();
@@ -102,4 +112,8 @@ public class HashRouters<S,T>{
 			addListElement(key,addHash.routers.get(key));
 		}
 	}
+	/**
+	 * @return the number of different MAC addresses in the hashTable
+	 */
+	public int getCountOfRouters(){ return countOfRouters; }
 }
--- src/main/java/components/Filters/TimePredicate.java
+++ src/main/java/components/Filters/TimePredicate.java
@@ -3,17 +3,36 @@ package components.Filters;
 import components.Attributes.WifiPointsTimePlace;
 
 import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.function.Predicate;
 
 public class TimePredicate implements Predicate<WifiPointsTimePlace> {
     private Date timeFrom;
     private Date timeTo;
+    DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
-    public TimePredicate(String timeFrom, String timeTo) {
 
-        this.timeFrom = new Date(timeFrom);
-        this.timeTo = new Date(timeTo);
+    public TimePredicate(String timeFromStr, String timeToStr) {
+
+        timeFromStr= timeFromStr.replace("T"," ");
+        timeToStr= timeToStr.replace("T"," ");
+
+        try {
+
+            Date timeFrom = formatter.parse(timeFromStr);
+            Date timeTo = formatter.parse(timeToStr);
+
+            System.out.println(timeFrom);
+            System.out.println(formatter.format(timeFrom));
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        this.timeFrom = timeFrom;
+        this.timeTo = timeTo;
     }
 
     @Override
